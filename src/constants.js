dc9b7573dea44c334e6008436697c88e1653a427
mlhotecky <nerxeus@gmail.com>
2021-07-21 11:02:48 +0200

    Added: constants in one file

---
+++ src/constants.js
@@ -0,0 +1,17 @@
+export const API_PREFIX = "http://localhost:3000/api/products";
+
+// types of actions
+export const GET_PRODUCT_STATUS = "GET_PRODUCT_STATUS";
+export const GET_PRODUCTS = "GET_PRODUCTS";
+export const UPDATE_PRODUCT = "UPDATE_PRODUCT";
+
+// status of api call
+export const PENDING = "PENDING";
+export const SUCCEEDED = "SUCCEEDED";
+export const REJECTED = "REJECTED";
+
+// messages for responses
+export const NO_DATA = "No data.";
+export const ERROR = "Error occurred:";
+export const ERROR_FETCHING = "Error occurred while fetching data.";
+export const PRODUCT_UPDATED = "Product has been updated!"
