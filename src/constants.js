bf7da16bb5cb97c2db803c1bb66f0dc574311557
mlhotecky <nerxeus@gmail.com>
2021-07-21 13:33:15 +0200

    Changed: constants.js - add utils for number validation in form and show currency by locale

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,6 @@
+import Cookies from "js-cookie";
+const lang = Cookies.get("lang");
+
 export const API_PREFIX = "http://localhost:3000/api/products";
 
 // types of actions
@@ -15,3 +18,9 @@ export const NO_DATA = "No data.";
 export const ERROR = "Error occurred:";
 export const ERROR_FETCHING = "Error occurred while fetching data.";
 export const PRODUCT_UPDATED = "Product has been updated!"
+
+// validation of invalid values
+
+export const invalidValues = [undefined, null, "", NaN];
+export const numberValidation = (val) => !invalidValues.includes(val) && val <= 0;
+export const localeCurrency = (val) => lang === "cs" ? `${val?.toLocaleString()},-` : `${val?.toLocaleString()} CZK`
