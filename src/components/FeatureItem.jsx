d799fe191319683a855c2972750e6c039a6d5e58
mlhotecky <nerxeus@gmail.com>
2021-07-21 10:57:13 +0200

    Added: feature item

---
+++ src/components/FeatureItem.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import {Col} from "react-flexbox-grid";
+import {Check} from '@material-ui/icons';
+
+export default function FeatureItem(props) {
+
+    const {feature} = props;
+
+    return (
+        <Col className="feature-item" md={12}>
+            <Check style={{color: "#66ff5b"}}/>
+            <em>{feature}</em>
+        </Col>
+    )
+}
