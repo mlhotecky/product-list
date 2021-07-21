a3af7a506daa1ab7628034338b97856658f51ff8
mlhotecky <nerxeus@gmail.com>
2021-07-21 10:53:11 +0200

    Added: header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Header() {
+    return (
+        <div className="app-header">
+            <div className="header-content">
+                <div>Eshop example</div>
+            </div>
+        </div>
+    )
+}
